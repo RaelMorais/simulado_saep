c38b927fc4b036c5b31c12e0413834e649db06cc
RaelMorais <israelsantanamorais@outlook.com>
2025-08-29 01:13:00 -0300

    alterando infos

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=255, default=' ')
     email = models.EmailField(max_length=255)
-    phone = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255, default='')
 
 
 # Task model for to-do 
